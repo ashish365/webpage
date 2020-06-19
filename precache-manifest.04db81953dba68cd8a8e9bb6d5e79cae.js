fab98d69d7f4beb311bf6142465b6dddc14aff8e
ashish365 <ashishraj365@gmail.com>
2020-06-20 00:18:02 +0530

    Updates

---
+++ precache-manifest.04db81953dba68cd8a8e9bb6d5e79cae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b05d4f837cdf65c8988a790d5d60dc",
+    "url": "/webpage/index.html"
+  },
+  {
+    "revision": "70a44a8449288eabdd40",
+    "url": "/webpage/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4cfbfecffbb15d09725b",
+    "url": "/webpage/static/js/2.0d5afb27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webpage/static/js/2.0d5afb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a44a8449288eabdd40",
+    "url": "/webpage/static/js/main.329a59be.chunk.js"
+  },
+  {
+    "revision": "318a061e1d2c2f3bda83",
+    "url": "/webpage/static/js/runtime-main.adf0b9a5.js"
+  },
+  {
+    "revision": "ed68be5098b23668db452da116b97340",
+    "url": "/webpage/static/media/iitg1.ed68be50.jpg"
+  },
+  {
+    "revision": "ac4e777224fcafebf03b2bdfac4a1bee",
+    "url": "/webpage/static/media/lionel-messi.ac4e7772.jpg"
+  },
+  {
+    "revision": "0610db91e50113aee912a28ed88800b4",
+    "url": "/webpage/static/media/ronaldo.0610db91.jpeg"
+  }
+]);
